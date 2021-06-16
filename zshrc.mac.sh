63a1d8e9a80555b6a5410e0e1508cf4967e95618
Eyal.Kobrigo <305776+kobrigo@users.noreply.github.com>
2021-06-16 16:46:03 +0300

    added the option on mac zshrc file to switch to vim when pressing v on command line

--- zshrc.mac.sh
+++ zshrc.mac.sh
@@ -10,3 +10,10 @@ bindkey -M viins 'jj' vi-cmd-mode
 bindkey '^R' history-incremental-search-backward
 #read all the aliases from my alieases file
 source $SCRIPT_PATH/alias.mac.sh
+
+# these 3 lines will make the clicking on v in normal mode to open the command line in vim to be edited
+# :wq will run the command
+export VISUAL=vim
+autoload edit-command-line; zle -N edit-command-line
+bindkey -M vicmd v edit-command-line
+
