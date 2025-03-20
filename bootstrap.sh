10d41f94b098a17764f7524b6e4c0b4cdc5f3d86
Eyal Kobrigo <305776+kobrigo@users.noreply.github.com>
2025-03-20 17:11:35 +0200

    changed the file name from bootstrp.sh to install.sh and made the script more explicit

--- bootstrap.sh
+++
@@ -1,5 +0,0 @@
-omz plugin load git
-unalias gp
-
-source zshrc.linux.sh
-
---
+++ install.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/zsh
+
+DOTFILES_DIR="${HOME}/.dotfiles"
+ZSH_CUSTOM_DIR="${HOME}/.oh-my-zsh/custom"
+
+source ${DOTFILES_DIR}/zshrc.linux.sh
+
