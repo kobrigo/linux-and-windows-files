03cd758f28c65788d7954478476553a63d5c6961
Eyal.Kobrigo <305776+kobrigo@users.noreply.github.com>
2026-04-04 02:28:45 +0300

    updated the backup script of RPI to use the hostname in the filename created and gzip it

--- backupFromRPItoNas/backup.sh
+++ backupFromRPItoNas/backup.sh
@@ -1,8 +1,13 @@
 #!/bin/zsh
 
-FILE="/mnt/nas/RpiBackups/sensor/backup-$(date +%F-%H%M).img"
+FILE="/mnt/nas/RpiBackups/sensor/backup-$(hostname)-$(date +%F-%H%M).img.gz"
+
 sudo sync
-sudo dd if=/dev/mmcblk0 bs=4M status=progress of="$FILE" conv=fsync
-# If you want to gzip the files while you back it up. Uncommang the next 2 lines instead of the line before this (didn't try it yet should work)
-#sudo dd if=/dev/mmcblk0 bs=4M status=progress | gzip -1 > "$FILE"
-#gzip -t "$FILE" && echo "Backup OK: $FILE"
+
+# Backup and compress on the fly
+sudo dd if=/dev/mmcblk0 bs=4M status=progress | gzip -1 > "$FILE"
+# use this instead if you don't want to zip
+# sudo dd if=/dev/mmcblk0 bs=4M status=progress of="$FILE" conv=fsync
+
+# Verify gzip integrity
+gzip -t "$FILE" && echo "Backup OK: $FILE"
